cb6afd7c527bedf9099fe50b9729f234bea7f809
KojoMensah <boit7845@gmail.com>
2023-07-29 22:18:04 +0000

    working on simplicity

---
+++ lib/features/authentication/domain/entities/user.dart
@@ -0,0 +1,6 @@
+class NoteUser{
+  final String name;
+  final String email;
+
+  NoteUser(this.name, this.email);
+}
---
+++ lib/features/note/domain/entities/note.dart
@@ -0,0 +1,34 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
+class Note {
+  final String id;
+  final String title;
+  final String noteBody;
+
+  Note({
+    required this.id,
+    required this.title,
+    required this.noteBody,
+  });
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'title': title,
+      'noteBody': noteBody,
+    };
+  }
+
+  factory Note.fromMap(Map<String, dynamic> map) {
+    return Note(
+      id: map['id'] as String,
+      title: map['title'] as String,
+      noteBody: map['noteBody'] as String,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Note.fromJson(String source) => Note.fromMap(json.decode(source) as Map<String, dynamic>);
+}
